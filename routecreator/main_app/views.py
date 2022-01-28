490b270511aa6f674e2183d5b6083a9da4890762
Programming <programming@Marcos-MacBook-Pro.local>
2022-01-28 14:24:50 -0800

    created django project

---
+++ routecreator/main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+# Create your views here.
+
+def about(request):
+    return render(request, 'about.html')
