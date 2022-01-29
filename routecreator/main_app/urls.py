c19962c3fadb53a4e154e597e869b64923b81ab6
Chris Chu <christopherjc12@gmail.com>
2022-01-28 21:24:20 -0800

    changed index to function view instead of CBV

--- routecreator/main_app/urls.py
+++ routecreator/main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('routes/', views.RouteList.as_view(), name='index'),
+    path('routes/', views.index, name='index'),
     path('routes/<int:route_id>/', views.routes_detail, name='detail'),
 ]
