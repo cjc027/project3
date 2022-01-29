2dd6b71fa8b854372a13760f7b23212a4351be92
Faith Johnson <faithpjohnson@gmail.com>
2022-01-28 17:55:27 -0700

    add detail page

--- routecreator/main_app/urls.py
+++ routecreator/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('route/', views.RouteList.as_view(), name='index'),
+    path('routes/', views.RouteList.as_view(), name='index'),
+    path('routes/<int:route_id>/', views.routes_detail, name='detail'),
 ]
