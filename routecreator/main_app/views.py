e015ff36d1689e9d1bd4066981c9551e5600ae5d
Joshua Cantrell <JoshCantrellsMac@Joshuas-MacBook-Pro-4.local>
2022-01-31 14:07:46 -0500

    fixed models and create view

--- routecreator/main_app/views.py
+++ routecreator/main_app/views.py
@@ -60,9 +60,13 @@ def signup(request):
 
 class RouteCreate (CreateView):
     model = Route
+<<<<<<< HEAD
     fields = fields = ['travel_distance', 'travel_hours', 'travel_minutes', 'date_created', 'country', 'state', 'city', 'description', 'name', 'mode_of_transport'] # referring the models field, so what fields do you want
 
 
+=======
+    fields = fields = ['name', 'mode_of_transport', 'travel_distance', 'country', 'state', 'city', 'travel_hours', 'travel_minutes',  'description'] # referring the models field, so what fields do you want
+>>>>>>> 18a2a58 (fixed models and create view)
     # to include on the form
 
     def form_valid(self, form):
