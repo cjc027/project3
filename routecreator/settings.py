1295d308dfc4c87bf19eb046471273c642a97b7b
Chris Chu <christopherjc12@gmail.com>
2022-02-03 16:58:04 -0800

    disable django_heroku

--- routecreator/settings.py
+++ routecreator/settings.py
@@ -132,5 +132,5 @@ LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
