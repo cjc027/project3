bebcbf6ccffdf1cbe1f81a8b0b132f59326603f8
Faith Johnson <faithpjohnson@gmail.com>
2022-02-03 17:44:52 -0700

    clean up

--- main_app/forms.py
+++ main_app/forms.py
@@ -3,13 +3,9 @@ from .models import Route, Comment
 
 
 class RouteForm(ModelForm):
-	#Meta describes data about the functionality 
-
 	class Meta:
 		model = Route
 		fields = ['country', 'state', 'city']
-
-
 
 
 class CommentForm(ModelForm):
